842c3c253624e40a201ff5e6b6be8ac19598c111
linxue <599562695@qq.com>
2022-03-19 10:11:22 +0800

    feat:PushGroupConstant

---
+++ src/Ecmall/PushGroupConstant.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace CrmConstant\Ecmall;
+
+/**
+ * 推送分组 常量
+ * @date 2022-03-19 10:11:18
+ */
+class PushGroupConstant
+{
+    
+    /**
+     * 分组1
+     */
+    const GROUP_ONE = 1;
+    
+    /**
+     * 分组2
+     */
+    const GROUP_TWO = 2;
+    
+    /**
+     * 分组3
+     */
+    const GROUP_THREE = 3;
+    
+    /**
+     * 分组4
+     */
+    const GROUP_FOUR = 4;
+    
+    /**
+     * 分组5
+     */
+    const GROUP_FIVE = 5;
+    
+    /**
+     * 分组6
+     */
+    const GROUP_SIX = 6;
+    
+    /**
+     * 分组7
+     */
+    const GROUP_SEVEN = 7;
+    
+    /**
+     * 分组8
+     */
+    const GROUP_EIGHT = 8;
+
+    /**
+     * 分组9
+     */
+    const GROUP_NINE = 9;
+    
+
+}
