c289b8c698f83f4703c51a1bb29236f73cfce2c1
linxue <599562695@qq.com>
2022-03-22 18:15:18 +0800

    feat:BmUriConstant

---
+++ src/Third/BmUriConstant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CrmConstant\Third;
+
+/**
+ * 决策王 网址常量
+ */
+class BmUriConstant
+{
+    /**
+     * 获取员工渠道码列表(海报图)
+    */
+    const CHANNEL_QR_CODE_LIST = "/oa/staff/channel-qr-code-list";
+
+
+}
